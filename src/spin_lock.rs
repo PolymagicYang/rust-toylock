ce3dcf4fa35570994f2569499c764640b38e55f4
PolymagicYang <PolymagicYang@protonmail.com>
2022-04-06 15:06:45 +0800

    not finished

--- src/spin_lock.rs
+++ src/spin_lock.rs
@@ -1,9 +1,9 @@
-use std::{sync::atomic::{ AtomicBool, Ordering }, ops::Deref};
+use std::{sync::{atomic::{ AtomicBool, Ordering }, Arc}, ops::{Deref, DerefMut}};
 // 自旋锁：持有锁的线程不断查看是否有其他线程在使用锁。
 
 // 因为数据本身是可传递和同步的，所以锁也应该是可传递和可同步的。
-unsafe impl<'s, T: Send + Sync> Send for LockGuard<'s, T> {}
-unsafe impl<'s, T: Send + Sync> Sync for LockGuard<'s, T> {}
+unsafe impl<T: Send + Sync> Send for SpinLock<T> {}
+unsafe impl<T: Send + Sync> Sync for SpinLock<T> {}
 
 struct RawLock {
 	is_hold: AtomicBool,
@@ -11,38 +11,58 @@ struct RawLock {
 
 // 特别声明一个生命周期，确保锁保护的数据不会存活在锁的周期以外.
 // T 必须可以被所有线程访问，或被发往别的线程.
-struct LockGuard<'s, T: Send + Sync> {
+pub struct LockGuard<'a, T: Send + Sync> {
+	data: &'a mut T,
+	lock: &'a RawLock,
+}
+
+pub struct SpinLock<T: Send + Sync> {
 	data: T,
 	lock: RawLock,
 }
 
-impl<'s, T: Send + Sync> LockGuard<'s, T> {
-	fn new(data: &'s T) -> Self {
-		LockGuard { data: &data, lock: RawLock { is_hold: AtomicBool::new(false) } }
+impl<T: Send + Sync> SpinLock<T> {
+	fn new(data: T) -> Self {
+		SpinLock { data, lock: RawLock { is_hold: AtomicBool::new(false) } }
 	}
 }
 
-impl<'s, T> Deref for LockGuard<'s, T> where T: Send + Sync {
+impl<T: Send + Sync> SpinLock<T> {
+	pub fn lock<'a>(self) -> LockGuard<'a, T> {
+		while self.lock.is_hold.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).is_err() {
+
+		}
+		LockGuard { data: &mut self.data, lock: &self.lock }
+	}
+}
+
+impl<'a, T: Send + Sync> LockGuard<'a, T> {
+	fn unlock(&self) {
+		self.lock.is_hold.swap(false, Ordering::Release);
+	}
+}
+
+impl<'a, T: Send + Sync> Deref for LockGuard<'a, T> {
 	type Target = T;
 	fn deref(&self) -> &Self::Target {
 		&*self.data
 	}
 }
 
-impl<'s, T: Send + Sync> LockGuard<'s, T> {
-	pub fn lock(&self) {
-		while self.lock.is_hold.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).is_err() {
-
-		}
-	}
-
-	pub fn unlock(&self) {
-		self.lock.is_hold.swap(false, Ordering::Release);
+impl<'a, T: Send + Sync> DerefMut for LockGuard<'a, T> {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut *self.data
 	}
 }
 
 #[test]
 fn test() {
-	let lock = LockGuard::new(0);
-	println!("Hello!");
+	let mut test = Arc::new(SpinLock::new(0));
+	for _ in 0..10 {
+		let test = test.clone();
+		std::thread::spawn(move || {
+			let guard = test.lock();
+			*guard += 1;
+		});
+	}
 }
