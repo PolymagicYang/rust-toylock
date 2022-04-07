dec180208c8c1a8ab22c30434c9473336c4afbd6
PolymagicYang <PolymagicYang@protonmail.com>
2022-04-07 20:30:18 +0800

    almost ready

--- src/spin_lock.rs
+++ src/spin_lock.rs
@@ -1,38 +1,43 @@
-use std::{sync::{atomic::{ AtomicBool, Ordering }, Arc}, ops::{Deref, DerefMut}};
+use std::{sync::{atomic::{ AtomicBool, Ordering }, Arc}, ops::{Deref, DerefMut}, cell::UnsafeCell};
 // 自旋锁：持有锁的线程不断查看是否有其他线程在使用锁。
 
 // 因为数据本身是可传递和同步的，所以锁也应该是可传递和可同步的。
 unsafe impl<T: Send + Sync> Send for SpinLock<T> {}
 unsafe impl<T: Send + Sync> Sync for SpinLock<T> {}
 
+#[derive(Debug)]
 struct RawLock {
 	is_hold: AtomicBool,
 }
 
 // 特别声明一个生命周期，确保锁保护的数据不会存活在锁的周期以外.
 // T 必须可以被所有线程访问，或被发往别的线程.
+#[derive(Debug)]
 pub struct LockGuard<'a, T: Send + Sync> {
 	data: &'a mut T,
 	lock: &'a RawLock,
 }
 
 pub struct SpinLock<T: Send + Sync> {
-	data: T,
+	data: UnsafeCell<T>,
 	lock: RawLock,
 }
 
 impl<T: Send + Sync> SpinLock<T> {
 	fn new(data: T) -> Self {
-		SpinLock { data, lock: RawLock { is_hold: AtomicBool::new(false) } }
+		SpinLock { data: UnsafeCell::new(data), lock: RawLock { is_hold: AtomicBool::new(false) } }
 	}
 }
 
-impl<T: Send + Sync> SpinLock<T> {
-	pub fn lock<'a>(self) -> LockGuard<'a, T> {
-		while self.lock.is_hold.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).is_err() {
+impl<'a, T: Send + Sync + Copy> SpinLock<T> {
+	pub fn lock(&'a self) -> LockGuard<'a, T> {
+		while self.lock.is_hold.compare_exchange(false, true, Ordering::Acquire, Ordering::Acquire).is_err() {
 
 		}
-		LockGuard { data: &mut self.data, lock: &self.lock }
+		LockGuard::<'a, _> { 
+			data: unsafe { &mut *self.data.get() as &mut T }, 
+			lock: &self.lock 
+		}
 	}
 }
 
@@ -57,12 +62,13 @@ impl<'a, T: Send + Sync> DerefMut for LockGuard<'a, T> {
 
 #[test]
 fn test() {
-	let mut test = Arc::new(SpinLock::new(0));
+	let test: &'static SpinLock<_> = Box::leak(Box::new(SpinLock::new(0)));
 	for _ in 0..10 {
-		let test = test.clone();
 		std::thread::spawn(move || {
-			let guard = test.lock();
+			let mut guard = test.lock();
 			*guard += 1;
+			guard.unlock();
 		});
 	}
+	println!("{}", test.lock().data);
 }
