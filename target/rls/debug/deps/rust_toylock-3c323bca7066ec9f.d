b59b828ba08cf8d551765613d83e3cd755c6001b
PolymagicYang <PolymagicYang@protonmail.com>
2022-04-10 16:51:35 +0800

    reorganize project structure

--- target/rls/debug/deps/rust_toylock-3c323bca7066ec9f.d
+++ target/rls/debug/deps/rust_toylock-3c323bca7066ec9f.d
@@ -1,7 +1,6 @@
-/home/magicyang/code/rust-toylock/target/rls/debug/deps/rust_toylock-3c323bca7066ec9f.rmeta: src/lib.rs src/spin_lock.rs src/ticket_lock.rs
+/home/magicyang/code/rust-toylock/target/rls/debug/deps/rust_toylock-3c323bca7066ec9f.rmeta: src/lib.rs src/spin_lock.rs
 
-/home/magicyang/code/rust-toylock/target/rls/debug/deps/rust_toylock-3c323bca7066ec9f.d: src/lib.rs src/spin_lock.rs src/ticket_lock.rs
+/home/magicyang/code/rust-toylock/target/rls/debug/deps/rust_toylock-3c323bca7066ec9f.d: src/lib.rs src/spin_lock.rs
 
 src/lib.rs:
 src/spin_lock.rs:
-src/ticket_lock.rs:
